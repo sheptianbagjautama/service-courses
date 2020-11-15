c6ae10cc8d3b05686f0dc9e69e3b6338aa144ded
sheptianbagja <sheptianbagjautama@gmail.com>
2020-11-15 12:47:51 +0700

    Troubleshoot Api MyCourse integrasi dengan services order payment midtrans

--- app/Http/Controllers/MyCourseController.php
+++ app/Http/Controllers/MyCourseController.php
@@ -72,12 +72,41 @@ class MyCourseController extends Controller
             ], 409);
         }
 
-        $myCourse = MyCourse::create($data);
+        if ($course->type === 'premium') {
+            if ($course->price === 0) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Price can\'t be 0'
+                ], 405);
+            }
 
-        return response()->json([
-            'status' => 'success',
-            'data' => $myCourse
-        ]);
+            $order = postOrder([
+                'user' => $user['data'],
+                'course' => $course->toArray()
+            ]);
+
+            //for debug
+            // echo "<pre>" . print_r($order, 1) . "</pre>";
+
+            if ($order['status'] === 'error') {
+                return response()->json([
+                    'status' => $order['status'],
+                    'message' => $order['message']
+                ], $order['http_code']);
+            }
+
+            return response()->json([
+                'status' => $order['status'],
+                'data' => $order['data']
+            ]);
+        } else {
+            $myCourse = MyCourse::create($data);
+
+            return response()->json([
+                'status' => 'success',
+                'data' => $myCourse
+            ]);
+        }
     }
 
     public function createPremiumAccess(Request $request)
--- app/helpers.php
+++ app/helpers.php
@@ -45,3 +45,20 @@ function getUserByIds($userIds = [])
         ];
     }
 }
+
+function postOrder($params)
+{
+    $url = env('SERVICE_ORDER_PAYMENT_URL') . 'api/orders';
+    try {
+        $response = Http::post($url, $params);
+        $data = $response->json();
+        $data['http_code']  = $response->getStatusCode();
+        return $data;
+    } catch (\Throwable $th) {
+        return [
+            'status' => 'error',
+            'http_code' => 500,
+            'message' => 'service user unavailable'
+        ];
+    }
+}
